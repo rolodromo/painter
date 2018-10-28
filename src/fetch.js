8ae78fccea79bad162d0755e2d09c9acc3fa17bb
guumaster <gustavoandresmarin@gmail.com>
2018-10-28 18:18:09 +0100

    fix api url

--- src/fetch.js
+++ src/fetch.js
@@ -1,9 +1,10 @@
 import rp from 'request-promise'
+import config from '../config'
 
 export const fetchContent = async ({ id, qs }) => {
   console.log('qs', id, qs)
   return rp.get({
-    uri: `http://localhost:3001/api/generators/table/${id}/json`,
+    uri: `${config.apiUrl}/api/generators/table/${id}/json`,
     qs: {
       ...qs,
       format: 'json'
