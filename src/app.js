3abf98f5c263b78389319e8c5f0ec736980ece42
guumaster <gustavoandresmarin@gmail.com>
2018-10-28 09:51:16 +0100

    clean logging

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import bytes from 'bytes'
 import express from 'express'
 import bodyParser from 'body-parser'
 
@@ -9,56 +10,38 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
-	const html = req.body.content
-  console.log('request', req.url, html)
+  const html = req.body.content
+  console.log(`> ${req.method} ${req.url}. length: ${bytes(html.length)}`)
   next()
 })
 
 app.post('/png', async (req, res) => {
-	const html = req.body.content
-	const browser = await getBrowser()
-	const page = await browser.newPage()
-
-  console.log('content', html.length)
-	await page.goto(`data:text/html,${html}`, { waitUntil: 'networkidle0' })
-  res.header('Content-Type', 'image/png')
-	res.send(await page.screenshot({
-    type: 'png',
-    fullPage: true,
-		landscape: true,
-		printBackground: true
-	}))
-})
-
-app.post('/pdf', async (req, res) => {
-	const html = req.body.content
-	const browser = await getBrowser()
-	const page = await browser.newPage()
-
-  console.log('content', html.length)
-	await page.goto(`data:text/html,${html}`, { waitUntil: 'networkidle0' })
-
-	res.setHeader('Content-Disposition', 'inline; filename="print.pdf"')
-	res.send(await page.pdf({
-		//path: 'print.pdf',
-		format: 'A4',
-		landscape: true,
-		printBackground: true
-	}))
-})
-
-app.get('/', async (req, res) => {
+  const html = req.body.content
   const browser = await getBrowser()
   const page = await browser.newPage()
 
-  const html = '<h1>HELLO WORLD</h1>'
-  await page.goto(`data:text/html,${html}`, { waitUntil: 'networkidle2' })
-
-  const screen = await page.screenshot({
-    type: 'png'
-  })
+  await page.goto(`data:text/html,${html}`, { waitUntil: 'networkidle0' })
   res.header('Content-Type', 'image/png')
-  res.send(screen)
+  res.send(await page.screenshot({
+    type: 'png',
+    fullPage: true,
+    printBackground: true
+  }))
+})
+
+app.post('/pdf', async (req, res) => {
+  const html = req.body.content
+  const browser = await getBrowser()
+  const page = await browser.newPage()
+
+  await page.goto(`data:text/html,${html}`, { waitUntil: 'networkidle0' })
+
+  res.setHeader('Content-Disposition', 'inline; filename="print.pdf"')
+  res.send(await page.pdf({
+    format: 'A4',
+    landscape: true,
+    printBackground: true
+  }))
 })
 
 export default app
